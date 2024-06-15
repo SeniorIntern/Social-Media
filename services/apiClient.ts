7992174aa31746f868a30174ff7a19dc34bdef30
SeniorIntern <tnikhil.official@gmail.com>
2024-06-16 03:21:33 +0545

    Fix env read issue

--- services/apiClient.ts
+++ services/apiClient.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+const baseURL =
+  process.env.NEXT_PUBLIC_BASE_URL ||
+  'https://twitmatchplay-server.onrender.com/api/v1';
 
 const apiClient = axios.create({
   baseURL: baseURL,
