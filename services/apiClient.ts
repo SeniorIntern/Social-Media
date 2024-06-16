46b44bc21f8163989ee132954328ccd90d292015
SeniorIntern <tnikhil.official@gmail.com>
2024-06-16 09:37:54 +0545

    Update axios configuration for production

--- services/apiClient.ts
+++ services/apiClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const apiClient = axios.create({
   baseURL: 'https://twitmatchplay-server.onrender.com/api/v1',
-  withCredentials: true // enable sending cookies with the request
+  withCredentials: true // enable sending cookies with the request,
 });
 
 export default apiClient;
